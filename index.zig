63290f26aeda8ca8b484e84507c41db0f7ff14f3
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2023-04-08 14:58:14 +1200

    Use member function syntax for tokenizer

--- index.zig
+++ index.zig
@@ -8,57 +8,55 @@ const file = @import("file.zig");
 const tokenizer = @import("tokenizer.zig");
 const hashtable = @import("hash_table.zig");
 
-const usage = \\
-\\Usage: index [file ...]
-\\
+const usage =
+    \\
+    \\Usage: index [file ...]
+    \\
 ;
 
 pub fn main() !void {
-	var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-	defer arena.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-	const allocator = arena.allocator();
+    const allocator = arena.allocator();
 
-	var args = try std.process.argsAlloc(allocator);
-	defer std.process.argsFree(allocator, args);
+    var args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-	if (args.len < 2)
-		{
-		std.debug.print("{s}", .{usage});
-		return;
-		}
+    if (args.len < 2) {
+        std.debug.print("{s}", .{usage});
+        return;
+    }
 
+    std.debug.print("{s} {s}\n", .{ args[0], args[1] });
 
-	std.debug.print("{s} {s}\n", .{args[0], args[1]});
-	
-	const doc = try file.slurp(allocator, args[1]);
+    const doc = try file.slurp(allocator, args[1]);
 
-	const tok = try allocator.create(tokenizer.Tokenizer);
-	tokenizer.init(tok, doc);
+    const tok = try allocator.create(tokenizer.Tokenizer);
+    tok.init(doc);
 
-	const dictionary = try allocator.create(hashtable.HashTable);
-	hashtable.init(dictionary);
+    const dictionary = try allocator.create(hashtable.HashTable);
+    hashtable.init(dictionary);
 
-	var doc_count: usize = 0;
-	while (true) {
-		const t = tokenizer.next(tok);
-		if (t.type == tokenizer.TokenType.eof) break;
-		if (t.type == tokenizer.TokenType.docno)
-			{
-			if (doc_count > 0 and doc_count % 1000 == 0)
-				std.debug.print("{d} Documents\n", .{doc_count});
-			doc_count += 1;
-			}
-		try hashtable.insert(dictionary, allocator, t.token);
-	}
+    var doc_count: usize = 0;
+    while (true) {
+        const t = tok.next();
+        if (t.type == tokenizer.TokenType.eof) break;
+        if (t.type == tokenizer.TokenType.docno) {
+            if (doc_count > 0 and doc_count % 1000 == 0)
+                std.debug.print("{d} Documents\n", .{doc_count});
+            doc_count += 1;
+        }
+        try hashtable.insert(dictionary, allocator, t.token);
+    }
 
-	std.debug.print("{s}\n", .{"Finished indexing"});
-	std.debug.print("{s}\n", .{"Writing indexing"});
+    std.debug.print("{s}\n", .{"Finished indexing"});
+    std.debug.print("{s}\n", .{"Writing indexing"});
 
-	const index_file = try std.fs.cwd().createFile(
-		"index.dat",
-		.{ .read = true },
-	);
-	defer index_file.close();
-	try hashtable.write(dictionary, index_file);
+    const index_file = try std.fs.cwd().createFile(
+        "index.dat",
+        .{ .read = true },
+    );
+    defer index_file.close();
+    try hashtable.write(dictionary, index_file);
 }
--- tokenizer.zig
+++ tokenizer.zig
@@ -7,110 +7,96 @@ const std = @import("std");
 
 pub const TokenType = enum { docno, word, eof };
 
-pub const Token = struct {
-	token: []const u8,
-	type: TokenType
-};
+pub const Token = struct { token: []const u8, type: TokenType };
 
 pub const Tokenizer = struct {
-	index: usize,
-	length: usize,
-	document: []const u8
+    index: usize,
+    doc: []const u8,
+
+    pub fn init(t: *Tokenizer, doc: []const u8) void {
+        t.index = 0;
+        t.doc = doc;
+    }
+
+    pub fn next(t: *Tokenizer) Token {
+        while (true) {
+            // Whitespace
+            while (t.index < t.doc.len and std.ascii.isWhitespace(t.doc[t.index])) {
+                t.index += 1;
+            }
+            // EOF
+            if (t.index == t.doc.len) {
+                break;
+            } else if (std.mem.startsWith(u8, t.doc[t.index..], "<DOCNO>")) {
+                t.index += std.mem.len("<DOCNO>");
+
+                while (t.index < t.doc.len and std.ascii.isWhitespace(t.doc[t.index]))
+                    t.index += 1;
+
+                var i: usize = 0;
+                while (i < 256 and i + t.index < t.doc.len and t.doc[t.index + i] != '<' and !std.ascii.isWhitespace(t.doc[t.index + i])) {
+                    i += 1;
+                }
+
+                const out = Token{
+                    .token = t.doc[t.index .. t.index + i],
+                    .type = TokenType.docno,
+                };
+
+                t.index += i;
+
+                return out;
+            }
+            // Ignored tags
+            else if (t.doc[t.index] == '<') {
+                t.index += 1;
+                while (t.index < t.doc.len and t.doc[t.index] != '>')
+                    t.index += 1;
+                t.index += 1;
+                continue;
+            }
+            // Number
+            else if (std.ascii.isDigit(t.doc[t.index])) {
+                var i: usize = 0;
+                while (i < 256 and i + t.index < t.doc.len and std.ascii.isDigit(t.doc[t.index + i])) {
+                    i += 1;
+                }
+
+                const out = Token{
+                    .token = t.doc[t.index .. t.index + i],
+                    .type = TokenType.word,
+                };
+
+                t.index += i;
+
+                return out;
+            }
+            // Word
+            else if (std.ascii.isAlpha(t.doc[t.index])) {
+                var i: usize = 0;
+                while (i < 256 and i + t.index < t.doc.len and std.ascii.isAlpha(t.doc[t.index + i])) {
+                    // buffer[i] = char_tolower(t.doc[t.index + i]);
+                    i += 1;
+                }
+
+                const out = Token{
+                    .token = t.doc[t.index .. t.index + i],
+                    .type = TokenType.word,
+                };
+
+                t.index += i;
+
+                return out;
+            }
+            // Something else we don't want
+            else {
+                t.index += 1;
+            }
+        }
+        const out = Token{
+            .token = t.doc[0..0],
+            .type = TokenType.eof,
+        };
+        return out;
+    }
 };
-
-pub fn init(t: *Tokenizer, document: []const u8) void {
-    t.index = 0;
-    t.length = document.len;
-    t.document = document;
-}
-
-pub fn next(t: *Tokenizer) Token {
-	while (true) {
-		// Whitespace
-		while (t.index < t.length and std.ascii.isWhitespace(t.document[t.index])) {
-			t.index += 1;
-		}
-		// EOF
-		if (t.index == t.length) {
-			break;
-		}
-		else if (std.mem.startsWith(u8, t.document[t.index..], "<DOCNO>")) {
-			t.index += std.mem.len("<DOCNO>");
-
-			while(t.index < t.length and std.ascii.isWhitespace(t.document[t.index]))
-				t.index += 1;
-
-			var i: usize = 0;
-			while (i < 256 and i + t.index < t.length and t.document[t.index + i] != '<' and !std.ascii.isWhitespace(t.document[t.index + i]))
-				{
-				i += 1;
-				}
-
-			const out = Token {
-				.token = t.document[t.index..t.index+i],
-				.type = TokenType.docno,
-				};
-
-			t.index += i;
-
-			return out;
-			}
-		// Ignored tags
-		else if (t.document[t.index] == '<')
-			{
-			t.index += 1;
-			while (t.index < t.length and t.document[t.index] != '>')
-				t.index += 1;
-			t.index += 1;
-			continue;
-			}
-		// Number
-		else if (std.ascii.isDigit(t.document[t.index]))
-			{
-			var i: usize = 0;
-			while (i < 256 and i + t.index < t.length and std.ascii.isDigit(t.document[t.index + i]))
-				{
-				i += 1;
-				}
-
-			const out = Token {
-				.token = t.document[t.index..t.index+i],
-				.type = TokenType.word,
-				};
-
-			t.index += i;
-
-			return out;
-			}
-		// Word
-		else if (std.ascii.isAlpha(t.document[t.index]))
-			{
-			var i: usize = 0;
-			while (i < 256 and i + t.index < t.length and std.ascii.isAlpha(t.document[t.index + i]))
-				{
-				// buffer[i] = char_tolower(t.document[t.index + i]);
-				i += 1;
-				}
-
-			const out = Token {
-				.token = t.document[t.index..t.index+i],
-				.type = TokenType.word,
-				};
-
-			t.index += i;
-
-			return out;
-			}
-		// Something else we don't want
-		else {
-			t.index += 1;
-		}
-
-	}
-	const out = Token {
-		.token = t.document[0..0],
-		.type = TokenType.eof,
-		};
-	return out;
-}
-

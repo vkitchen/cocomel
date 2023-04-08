9d6c38d76578ec756ba2e9dfe32e0e27c47dd701
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2023-04-08 15:47:06 +1200

    Simplify slurp

--- file.zig
+++ file.zig
@@ -5,16 +5,9 @@
 
 const std = @import("std");
 
-pub fn slurp(allocator: std.mem.Allocator, filename: []u8) ![]u8
-	{
-	var file = try std.fs.cwd().openFile(filename, .{});
-	defer file.close();
+pub fn slurp(allocator: std.mem.Allocator, filename: []u8) ![]u8 {
+    var file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
 
-	const stat = try file.stat();
-	std.debug.print("FILE SIZE: {d}\n", .{stat.size});
-
-	const buffer = try allocator.alloc(u8, stat.size);
-	_ = try file.readAll(buffer); // TODO did we read entire file?
-
-	return buffer;
-	}
+    return file.readToEndAlloc(allocator, std.math.maxInt(usize));
+}

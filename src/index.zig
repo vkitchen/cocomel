db91e5d7c83de7b2c0ab077b65c171cd0959fc1e
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2023-04-15 13:55:00 +1200

    Index as struct

--- src/index.zig
+++ src/index.zig
@@ -12,65 +12,99 @@ pub const Result = struct {
     score: f64,
 };
 
-pub fn read32(index: []const u8, offset: usize) u32 {
-    return std.mem.bytesToValue(u32, index[offset .. offset + @sizeOf(u32)][0..4]);
+fn read32(str: []const u8, offset: usize) u32 {
+    return std.mem.bytesToValue(u32, str[offset .. offset + @sizeOf(u32)][0..4]);
 }
 
-pub fn name(index: []const u8, offset: u32, doc_id: u32) []const u8 {
-    const docs_start = offset + @sizeOf(u32);
+pub const Index = struct {
+    const Self = @This();
 
-    const stride = doc_id * @sizeOf(u32);
-    const name_offset = read32(index, docs_start + stride) + @sizeOf(u32);
-    const name_length = read32(index, name_offset);
-    const name_start = name_offset + @sizeOf(u32);
-    return index[name_start .. name_start + name_length];
-}
+    index: []const u8,
+    docs_offset: u32,
+    hash_offset: u32,
+    docs_count: u32,
+    average_length: f64,
 
-fn doc_length(index: []const u8, offset: u32, doc_id: u32) u32 {
-    const docs_start = offset + @sizeOf(u32);
+    pub fn init(index: []const u8) Self {
+        const hash_offset = read32(index, index.len - 4);
+        const docs_offset = read32(index, index.len - 8);
 
-    const stride = doc_id * @sizeOf(u32);
-    const length_offset = read32(index, docs_start + stride);
-    return read32(index, length_offset);
-}
+        const docs_count = read32(index, docs_offset);
 
-fn postings(index: []const u8, offset: u32, docs_offset: u32, ranker: *Ranker, results: []Result) void {
-    const ids_offset = read32(index, offset);
-    const ids_len = read32(index, ids_offset);
+        var average_length: f64 = 0;
+        const docs_start = docs_offset + @sizeOf(u32);
+        var i: u32 = 0;
+        while (i < docs_count) : (i += 1) {
+            const name_offset = read32(index, docs_start + i * @sizeOf(u32));
+            average_length += @intToFloat(f64, read32(index, name_offset));
+        }
+        average_length /= @intToFloat(f64, docs_count);
 
-    const scores_offset = read32(index, offset + @sizeOf(u32));
-    const scores_len = read32(index, scores_offset);
-    const scores = scores_offset + @sizeOf(u32);
-
-    ranker.compIdf(@intToFloat(f64, scores_len));
-
-    var i: u32 = 0;
-    while (i < ids_len) : (i += 1) {
-        const stride = @sizeOf(u32) + i * @sizeOf(u32);
-        const doc_id = read32(index, ids_offset + stride);
-        const doc_len = doc_length(index, docs_offset, doc_id);
-        results[doc_id].score += ranker.compScore(@intToFloat(f64, index[scores + i]), @intToFloat(f64, doc_len));
+        return .{
+            .index = index,
+            .docs_offset = docs_offset,
+            .hash_offset = hash_offset,
+            .docs_count = docs_count,
+            .average_length = average_length,
+        };
     }
-}
 
-pub fn find(index: []const u8, offset: u32, docs_offset: u32, key: []const u8, ranker: *Ranker, results: []Result) void {
-    const cap = read32(index, offset);
-    const table = offset + @sizeOf(u32);
+    pub fn name(this: *const Self, doc_id: u32) []const u8 {
+        const docs_start = this.docs_offset + @sizeOf(u32);
 
-    var i = hash(key, cap);
-    while (true) {
-        const posting_offset = table + i * @sizeOf(u32);
-
-        const posting = read32(index, posting_offset);
-        if (posting == 0)
-            return;
-        const term_store = read32(index, posting);
-        const term_length = read32(index, term_store);
-        const term_start = term_store + @sizeOf(u32);
-        const term = index[term_start .. term_start + term_length];
-        if (std.mem.eql(u8, term, key))
-            return postings(index, posting + @sizeOf(u32), docs_offset, ranker, results);
-
-        i = i + 1 & (cap - 1);
+        const stride = doc_id * @sizeOf(u32);
+        const name_offset = read32(this.index, docs_start + stride) + @sizeOf(u32);
+        const name_length = read32(this.index, name_offset);
+        const name_start = name_offset + @sizeOf(u32);
+        return this.index[name_start .. name_start + name_length];
     }
-}
+
+    fn doc_length(this: *const Self, offset: u32, doc_id: u32) u32 {
+        const docs_start = offset + @sizeOf(u32);
+
+        const stride = doc_id * @sizeOf(u32);
+        const length_offset = read32(this.index, docs_start + stride);
+        return read32(this.index, length_offset);
+    }
+
+    fn postings(this: *const Self, offset: u32, ranker: *Ranker, results: []Result) void {
+        const ids_offset = read32(this.index, offset);
+        const ids_len = read32(this.index, ids_offset);
+
+        const scores_offset = read32(this.index, offset + @sizeOf(u32));
+        const scores_len = read32(this.index, scores_offset);
+        const scores = scores_offset + @sizeOf(u32);
+
+        ranker.compIdf(@intToFloat(f64, scores_len));
+
+        var i: u32 = 0;
+        while (i < ids_len) : (i += 1) {
+            const stride = @sizeOf(u32) + i * @sizeOf(u32);
+            const doc_id = read32(this.index, ids_offset + stride);
+            const doc_len = this.doc_length(this.docs_offset, doc_id);
+            results[doc_id].score += ranker.compScore(@intToFloat(f64, this.index[scores + i]), @intToFloat(f64, doc_len));
+        }
+    }
+
+    pub fn find(this: *const Self, key: []const u8, ranker: *Ranker, results: []Result) void {
+        const cap = read32(this.index, this.hash_offset);
+        const table = this.hash_offset + @sizeOf(u32);
+
+        var i = hash(key, cap);
+        while (true) {
+            const posting_offset = table + i * @sizeOf(u32);
+
+            const posting = read32(this.index, posting_offset);
+            if (posting == 0)
+                return;
+            const term_store = read32(this.index, posting);
+            const term_length = read32(this.index, term_store);
+            const term_start = term_store + @sizeOf(u32);
+            const term = this.index[term_start .. term_start + term_length];
+            if (std.mem.eql(u8, term, key))
+                return this.postings(posting + @sizeOf(u32), ranker, results);
+
+            i = i + 1 & (cap - 1);
+        }
+    }
+};
--- src/search.zig
+++ src/search.zig
@@ -5,13 +5,14 @@
 
 const std = @import("std");
 const file = @import("file.zig");
-const index = @import("index.zig");
+const Index = @import("index.zig").Index;
+const Result = @import("index.zig").Result;
 const tokenizer = @import("tokenizer.zig");
 const QueryTokenizer = tokenizer.QueryTokenizer;
 const Token = tokenizer.Token;
 const Ranker = @import("ranking_fn.zig").Ranker;
 
-fn cmpResults(context: void, a: index.Result, b: index.Result) bool {
+fn cmpResults(context: void, a: Result, b: Result) bool {
     return std.sort.desc(f64)(context, a.score, b.score);
 }
 
@@ -25,26 +26,15 @@ pub fn main() !void {
     const index_file = try file.slurp(allocator, "index.dat");
     std.debug.print("Index size {d}\n", .{index_file.len});
 
-    const docs_offset = index.read32(index_file, index_file.len - 8);
-    const hash_offset = index.read32(index_file, index_file.len - 4);
+    const index = Index.init(index_file);
 
-    const docs_count = index.read32(index_file, docs_offset);
-    std.debug.print("No. docs {d}\n", .{docs_count});
+    std.debug.print("No. docs {d}\n", .{index.docs_count});
 
-    var average_length: f64 = 0;
-    const docs_start = docs_offset + @sizeOf(u32);
+    var ranker = Ranker.init(@intToFloat(f64, index.docs_count), index.average_length);
+
+    var results = try allocator.alloc(Result, index.docs_count);
     var i: u32 = 0;
-    while (i < docs_count) : (i += 1) {
-        const name_offset = index.read32(index_file, docs_start + i * @sizeOf(u32));
-        average_length += @intToFloat(f64, index.read32(index_file, name_offset));
-    }
-    average_length /= @intToFloat(f64, docs_count);
-
-    var ranker = Ranker.init(@intToFloat(f64, docs_count), average_length);
-
-    var results = try allocator.alloc(index.Result, docs_count);
-    i = 0;
-    while (i < docs_count) : (i += 1) {
+    while (i < index.docs_count) : (i += 1) {
         results[i].doc_id = i;
         results[i].score = 0;
     }
@@ -60,11 +50,11 @@ pub fn main() !void {
     while (true) {
         const t = tok.next();
         if (t.type == Token.Type.eof) break;
-        index.find(index_file, hash_offset, docs_offset, t.token, &ranker, results);
+        index.find(t.token, &ranker, results);
     }
 
     std.debug.print("Searching: {s}\n", .{input.?});
-    std.sort.sort(index.Result, results, {}, cmpResults);
+    std.sort.sort(Result, results, {}, cmpResults);
 
     var results_count: u32 = 0;
     for (results) |result| {
@@ -78,6 +68,6 @@ pub fn main() !void {
 
     i = 0;
     while (i < std.math.min(10, results_count)) : (i += 1) {
-        std.debug.print("{s} Score: {d:.4}\n", .{ index.name(index_file, docs_offset, results[i].doc_id), results[i].score });
+        std.debug.print("{s} Score: {d:.4}\n", .{ index.name(results[i].doc_id), results[i].score });
     }
 }

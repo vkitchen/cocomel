d90febc83af3d1ee1bd772fa766d7762d68b1fa3
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2023-04-25 17:32:19 +1200

    Split tar parser and html parser

---
+++ src/tokenizer_html.zig
@@ -0,0 +1,115 @@
+//	TOKENIZER_HTML.ZIG
+//	------------------
+//	Copyright (c) Vaughan Kitchen
+//	Released under the ISC license (https://opensource.org/licenses/ISC)
+
+const std = @import("std");
+const Indexer = @import("indexer.zig").Indexer;
+
+pub fn HtmlTokenizer(comptime ReaderType: type) type {
+    return struct {
+        const Self = @This();
+
+        indexer: *Indexer,
+        doc: *ReaderType = undefined,
+        buf: [512]u8 = undefined, // Tar block size
+        index: usize = 0,
+        len: usize = 0,
+        file_size: usize = 0,
+
+        pub fn init(indexer: *Indexer) Self {
+            return .{ .indexer = indexer };
+        }
+
+        fn read(self: *Self) !void {
+            self.len = try self.doc.read(&self.buf);
+            self.index = 0;
+        }
+
+        fn eof(self: *Self) !bool {
+            if (self.file_size == 0)
+                return true;
+            if (self.index >= self.len)
+                try self.read();
+            return self.len == 0;
+        }
+
+        fn peek(self: *Self) !u8 {
+            if (try self.eof())
+                return 0;
+            return self.buf[self.index];
+        }
+
+        fn consume(self: *Self) void {
+            self.index += 1;
+            self.file_size -|= 1;
+        }
+
+        fn consumeWhitespace(self: *Self) !void {
+            while (std.ascii.isWhitespace(try self.peek()))
+                self.consume();
+        }
+
+        fn consumeStr(self: *Self, str: []const u8) !bool {
+            for (str) |c| {
+                if (try self.peek() == c) {
+                    self.consume();
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        pub fn tokenize(self: *Self, doc: *ReaderType, file_size: u64) !void {
+            self.doc = doc;
+            self.file_size = file_size;
+            try self.read();
+
+            while (true) {
+                const char = try self.peek();
+                // EOF
+                if (char == 0) {
+                    return;
+                }
+                // Tag
+                else if (char == '<') {
+                    self.consume();
+                    if (try self.peek() == 's') {
+                        self.consume();
+                        if (try self.consumeStr("cript") or try self.consumeStr("tyle")) {
+                            while (try self.peek() != '<')
+                                self.consume();
+                        }
+                    }
+                    while (try self.peek() != '>')
+                        self.consume();
+                    continue;
+                }
+                // Number
+                else if (std.ascii.isDigit(char)) {
+                    var i: usize = 0;
+                    while (i < self.indexer.buffer.len and std.ascii.isDigit(try self.peek())) : (i += 1) {
+                        self.indexer.buffer[i] = try self.peek();
+                        self.consume();
+                    }
+
+                    try self.indexer.addTerm(self.indexer.buffer[0..i]);
+                    continue;
+                }
+                // Word
+                else if (std.ascii.isAlpha(char)) {
+                    var i: usize = 0;
+                    while (i < self.indexer.buffer.len and std.ascii.isAlpha(try self.peek())) : (i += 1) {
+                        self.indexer.buffer[i] = try self.peek();
+                        self.consume();
+                    }
+
+                    try self.indexer.addTerm(self.indexer.buffer[0..i]);
+                    continue;
+                }
+                self.consume();
+            }
+        }
+    };
+}
--- src/tokenizer_tar.zig
+++ src/tokenizer_tar.zig
@@ -5,10 +5,20 @@
 
 const std = @import("std");
 const Indexer = @import("indexer.zig").Indexer;
-const Token = @import("tokenizer.zig").Token;
+const HtmlTokenizer = @import("tokenizer_html.zig").HtmlTokenizer;
+
+fn rtrim(str: []u8) []u8 {
+    var end = str.len;
+    while (end > 0) : (end -= 1)
+        if (str[end - 1] != 0)
+            break;
+    return str[0..end];
+}
 
 const TarHeader = extern struct {
-    name: [99:0]u8,
+    const Self = @This();
+
+    name: [100]u8,
     mode: [8]u8,
     uid: [8]u8,
     gid: [8]u8,
@@ -26,6 +36,21 @@ const TarHeader = extern struct {
     prefix: [155]u8,
 
     pad: [12]u8, // unused
+
+    fn fileSize(self: *Self) !u64 {
+        return try std.fmt.parseUnsigned(u64, &self.size, 8);
+    }
+
+    fn fullName(self: *Self, buffer: []u8) []u8 {
+        const name = rtrim(&self.name);
+        if (self.prefix[0] == 0)
+            return name;
+        const prefix = rtrim(&self.prefix);
+        std.mem.copy(u8, buffer, prefix);
+        buffer[prefix.len] = '/';
+        std.mem.copy(u8, buffer[prefix.len + 1 ..], name);
+        return buffer[0 .. prefix.len + 1 + name.len];
+    }
 };
 
 pub fn TarTokenizer(comptime ReaderType: type) type {
@@ -33,160 +58,39 @@ pub fn TarTokenizer(comptime ReaderType: type) type {
         const Self = @This();
 
         indexer: *Indexer,
-        bytes_consumed: usize = 0,
-        next_header: usize = 0,
-        index: usize = 0,
-        len: usize = 0,
-        buf: [4096]u8 = undefined,
-        name_buf: [255]u8 = undefined,
+        buf: [512]u8 = undefined, // Tar block size
+        name_buf: [256]u8 = undefined,
         doc: ReaderType,
-        should_exit: bool = false,
+        toker: HtmlTokenizer(ReaderType) = undefined,
 
         pub fn init(indexer: *Indexer, doc: ReaderType) Self {
-            return .{ .indexer = indexer, .doc = doc };
-        }
-
-        fn read(self: *Self) !void {
-            self.len = try self.doc.read(&self.buf);
-            self.index = 0;
-        }
-
-        fn getChar(self: *Self) !u8 {
-            if (self.index == self.len)
-                try self.read();
-            const char = self.buf[self.index];
-            self.index += 1;
-            self.bytes_consumed += 1;
-            return char;
-        }
-
-        fn peek(self: *Self) !u8 {
-            if (self.index == self.len)
-                try self.read();
-            return self.buf[self.index];
-        }
-
-        fn eof(self: *Self) !bool {
-            if (self.index == self.len)
-                try self.read();
-            return self.len == 0;
-        }
-
-        fn isHeader(self: *Self) !bool {
-            if (self.index == self.len)
-                try self.read();
-            return self.len != 0 and self.next_header == self.bytes_consumed;
-        }
-
-        fn getDocId(self: *Self) !?[]u8 {
-            // Header is 512 byte aligned. Can just read
-            const header: *TarHeader = @ptrCast(*TarHeader, self.buf[self.index..]);
-            self.index += @sizeOf(TarHeader);
-            self.bytes_consumed += @sizeOf(TarHeader);
-            self.next_header += @sizeOf(TarHeader);
-
-            if (header.typeflag == 0) {
-                self.should_exit = true;
-                return null;
-            }
-
-            if (header.typeflag == '5')
-                return null;
-
-            // TODO return this as an error
-            if (header.typeflag != '0') {
-                std.debug.print("ERROR: Unknown filetype '{c}' in tarball\n", .{header.typeflag});
-                std.process.exit(1);
-            }
-
-            // Find next header
-            const file_size = try std.fmt.parseUnsigned(u64, &header.size, 8);
-            self.next_header += file_size;
-            if (file_size % 512 != 0)
-                self.next_header += 512 - (file_size % 512);
-
-            // Extract full name
-            var name = @ptrCast([]u8, std.mem.span(@ptrCast([*:0]u8, &header.name)));
-            if (header.prefix[0] != 0) {
-                var prefix = std.mem.span(@ptrCast([*:0]u8, &header.prefix));
-                std.mem.copy(u8, &self.name_buf, prefix);
-                self.name_buf[prefix.len] = '/';
-                std.mem.copy(u8, self.name_buf[prefix.len + 1 ..], name);
-                name = self.name_buf[0 .. prefix.len + 1 + name.len];
-            }
-
-            return name;
+            return .{
+                .indexer = indexer,
+                .doc = doc,
+                .toker = HtmlTokenizer(ReaderType).init(indexer),
+            };
         }
 
         pub fn tokenize(self: *Self) !void {
             while (true) {
-                // Doc ID
-                if (try self.isHeader()) {
-                    const name = try self.getDocId();
-                    // Empty file type
-                    if (name == null) {
-                        if (self.should_exit)
-                            return;
-                        continue;
-                    }
-                    try self.indexer.addDocId(name.?);
-                    continue;
-                }
+                if (try self.doc.read(&self.buf) != 512)
+                    return;
 
-                const char = try self.getChar();
-                // EOF
-                if (char == 0) {
-                    if (try self.eof())
-                        return;
-                    continue;
-                }
-                // Whitespace
-                else if (std.ascii.isWhitespace(char)) {
-                    continue;
-                }
-                // Ignored tags
-                else if (char == '<') {
-                    if (!try self.eof() and try self.getChar() == 's') {
-                        const nextChar = try self.getChar();
-                        if (nextChar == 'c' and try self.getChar() == 'r' and try self.getChar() == 'i' and try self.getChar() == 'p' and try self.getChar() == 't') {
-                            while (!try self.eof() and try self.peek() != '<')
-                                _ = try self.getChar();
-                        }
-                        if (nextChar == 't' and try self.getChar() == 'y' and try self.getChar() == 'l' and try self.getChar() == 'e') {
-                            while (!try self.eof() and try self.peek() != '<')
-                                _ = try self.getChar();
-                        }
-                    }
-                    while (!try self.eof() and try self.peek() != '>')
-                        _ = try self.getChar();
-                    continue;
-                }
-                // Number
-                else if (std.ascii.isDigit(char)) {
-                    self.indexer.buffer[0] = char;
+                const header: *TarHeader = @ptrCast(*TarHeader, &self.buf);
 
-                    var i: usize = 1;
-                    while (i < self.indexer.buffer.len and !try self.eof() and std.ascii.isDigit(try self.peek())) : (i += 1)
-                        self.indexer.buffer[i] = try self.getChar();
+                _ = switch (header.typeflag) {
+                    0 => return,
+                    '0' => {},
+                    '5' => continue,
+                    else => std.debug.panic("unimplemented", .{}),
+                };
 
-                    try self.indexer.addTerm(self.indexer.buffer[0..i]);
+                try self.indexer.addDocId(header.fullName(&self.name_buf));
+
+                const file_size = try header.fileSize();
+                if (file_size == 0)
                     continue;
-                }
-                // Word
-                else if (std.ascii.isAlpha(char)) {
-                    self.indexer.buffer[0] = char;
-
-                    var i: usize = 1;
-                    while (i < self.indexer.buffer.len and !try self.eof() and (std.ascii.isAlpha(try self.peek()) or try self.peek() == '\'')) : (i += 1)
-                        self.indexer.buffer[i] = try self.getChar();
-                    if (try self.peek() == ',' or try self.peek() == '.') {
-                        self.indexer.buffer[i] = try self.getChar();
-                        i += 1;
-                    }
-
-                    try self.indexer.addTerm(self.indexer.buffer[0..i]);
-                    continue;
-                }
+                try self.toker.tokenize(&self.doc, file_size);
             }
         }
     };

313b2d4f310fa4bb496a2de1070a42b83be502c0
Vaughan Kitchen <contact@tophat.nz>
2026-06-01 15:54:23 +1200

    Include search time in output

--- src/prog_search.zig
+++ src/prog_search.zig
@@ -21,7 +21,13 @@ pub fn main(init: std.process.Init) !void {
 
     const query = try stdin.interface.takeDelimiter('\n');
 
+    const start_time = std.Io.Clock.now(.real, init.io).toNanoseconds();
+
     const results = try searcher.search(query.?);
+
+    const search_time = std.Io.Clock.now(.real, init.io).toNanoseconds();
+
+    std.debug.print("Search took {d:.3}\n", .{@as(f64, @floatFromInt(search_time - start_time)) / 1e9});
 
     std.debug.print("Top 10 Results ({d} total):\n\n", .{results.len});
 

267b0c2914240040b48ed32b48df2b81dca3f5f7
Vaughan Kitchen <contact@tophat.nz>
2026-06-03 21:03:00 +1200

    File keeps track of position for us

--- src/serialiser_ccml.zig
+++ src/serialiser_ccml.zig
@@ -49,7 +49,7 @@ pub const CcmlSerialiser = struct {
         try self.snippet_indices.append(allocator, @truncate(self.writer.logicalPos()));
     }
 
-    fn writeDictionary(self: *Self, allocator: std.mem.Allocator, h: *Dictionary, bytes_written: *u32) !u32 {
+    fn writeDictionary(self: *Self, allocator: std.mem.Allocator, h: *Dictionary) !u64 {
         const offsets = try allocator.alloc([2]u32, h.cap);
         @memset(offsets, .{ 0, 0 });
 
@@ -59,15 +59,13 @@ pub const CcmlSerialiser = struct {
                 const posting = p.?;
                 try posting.flush();
 
-                const term_offset = bytes_written.*;
+                const term_offset = self.writer.logicalPos();
                 try self.writer.interface.writeInt(u16, @truncate(posting.term.len), native_endian);
                 try self.writer.interface.writeAll(posting.term);
-                bytes_written.* += @sizeOf(u16) + @as(u32, @truncate(posting.term.len));
 
                 // df_t
-                const ids_offset = bytes_written.*;
+                const ids_offset = self.writer.logicalPos();
                 try self.writer.interface.writeInt(u32, @truncate(posting.df_t), native_endian);
-                bytes_written.* += @sizeOf(u32);
                 // postings chunks
                 var i: u8 = 255;
                 while (i > 0) : (i -= 1) {
@@ -77,28 +75,23 @@ pub const CcmlSerialiser = struct {
                     try self.writer.interface.writeInt(u32, @truncate(postings_list.items.len), native_endian);
                     try self.writer.interface.writeInt(u8, i, native_endian);
                     try self.writer.interface.writeAll(postings_list.items);
-                    bytes_written.* += @sizeOf(u32) + @sizeOf(u8) + @as(u32, @truncate(postings_list.items.len));
                 }
                 // If there is no impact 1 write a dummy postings list
-                if (posting.ids[0] == null) {
+                if (posting.ids[0] == null)
                     try self.writer.interface.writeInt(u32, 0, native_endian);
-                    bytes_written.* += @sizeOf(u32);
-                }
 
-                offsets[hi][0] = term_offset;
-                offsets[hi][1] = ids_offset;
+                offsets[hi][0] = @truncate(term_offset);
+                offsets[hi][1] = @truncate(ids_offset);
             }
         }
 
         // Write table
-        const table_offset = bytes_written.*;
+        const table_offset = self.writer.logicalPos();
         try self.writer.interface.writeInt(u32, h.cap, native_endian);
-        bytes_written.* += @sizeOf(u32);
 
         for (offsets) |p| {
             try self.writer.interface.writeInt(u32, p[0], native_endian);
             try self.writer.interface.writeInt(u32, p[1], native_endian);
-            bytes_written.* += 2 * @sizeOf(u32);
         }
 
         std.debug.print("Terms count {d}\n", .{h.len});
@@ -106,14 +99,13 @@ pub const CcmlSerialiser = struct {
         return table_offset;
     }
 
-    pub fn write(self: *Self, allocator: std.mem.Allocator, docs: *std.ArrayList(Doc), dictionary: *Dictionary) !u32 {
+    pub fn write(self: *Self, allocator: std.mem.Allocator, docs: *std.ArrayList(Doc), dictionary: *Dictionary) !u64 {
         // Flush snippets
         try self.newDocId(allocator);
 
-        var bytes_written: u32 = @truncate(self.writer.logicalPos());
-
         // Document ID strings
         for (docs.items, 0..) |d, i| {
+            const name_offset = self.writer.logicalPos();
             try self.writer.interface.writeInt(u32, d.len, native_endian);
             try self.writer.interface.writeInt(u16, @truncate(d.name.len), native_endian);
             try self.writer.interface.writeAll(d.name);
@@ -123,44 +115,35 @@ pub const CcmlSerialiser = struct {
             } else {
                 try self.writer.interface.writeInt(u16, 0, native_endian);
             }
-            docs.items[i].name.ptr = @ptrFromInt(bytes_written);
-            bytes_written += @sizeOf(u32) + @sizeOf(u16) + @as(u32, @truncate(d.name.len)) + @sizeOf(u16);
-            if (d.title) |title|
-                bytes_written += @truncate(title.len);
+            docs.items[i].name.ptr = @ptrFromInt(name_offset);
         }
 
         // Document IDs array
-        const docs_offset = bytes_written;
-        for (docs.items) |d| {
+        const docs_offset = self.writer.logicalPos();
+        for (docs.items) |d|
             try self.writer.interface.writeInt(u32, @truncate(@intFromPtr(d.name.ptr)), native_endian);
-            bytes_written += @sizeOf(u32);
-        }
 
         // Dictionary
-        const dictionary_offset = try self.writeDictionary(allocator, dictionary, &bytes_written);
+        const dictionary_offset = try self.writeDictionary(allocator, dictionary);
 
         // Snippets
-        const snippets_offset = bytes_written;
-        for (self.snippet_indices.items) |s| {
+        const snippets_offset = self.writer.logicalPos();
+        for (self.snippet_indices.items) |s|
             try self.writer.interface.writeInt(u32, s, native_endian);
-            bytes_written += @sizeOf(u32);
-        }
 
         // Metadata
         try self.writer.interface.writeInt(u32, @truncate(docs.items.len), native_endian);
-        try self.writer.interface.writeInt(u32, docs_offset, native_endian);
-        try self.writer.interface.writeInt(u32, dictionary_offset, native_endian);
-        try self.writer.interface.writeInt(u32, snippets_offset, native_endian);
-        bytes_written += 4 * @sizeOf(u32);
+        try self.writer.interface.writeInt(u32, @truncate(docs_offset), native_endian);
+        try self.writer.interface.writeInt(u32, @truncate(dictionary_offset), native_endian);
+        try self.writer.interface.writeInt(u32, @truncate(snippets_offset), native_endian);
 
         // Config
         try self.writer.interface.writeByte(0); // padding
         try self.writer.interface.writeByte(if (self.snippets) 1 else 0);
         try self.writer.interface.writeInt(u16, config.index_version, native_endian);
-        bytes_written += 4;
 
         try self.writer.flush();
 
-        return bytes_written;
+        return self.writer.logicalPos();
     }
 };
